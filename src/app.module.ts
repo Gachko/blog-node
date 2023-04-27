6d6458b343c3202ed6a5d2666ae5a6668701b297
Katsiaryna <k.hachko@godeltech.com>
2023-04-27 14:33:57 +0200

    add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostsModule } from './posts/posts.module';
+import { PrismaModule } from './common/prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { CommentModule } from './comment/comment.module';
+import { TagModule } from './tag/tag.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [PostsModule, PrismaModule, AuthModule, CommentModule, TagModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
